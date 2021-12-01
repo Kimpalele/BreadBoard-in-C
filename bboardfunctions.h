07d6a74f155ad6aff052e97990ee5a5dcec4f8ad
Kimpalele <kim99007@gmail.com>
2021-12-01 10:51:43 +0100

    Create bboardfunctions.h

---
+++ bboardfunctions.h
@@ -0,0 +1,8 @@
+#ifndef BBOARDFUNCTIONS_H
+#define BBOARDFUNCTIONS_H
+
+void printBreadboard(char** bboard, int rows, int columns);
+double addResistor(char** bboard, char count, int rows, int columns);
+int removeResistor(char** bboard, int rows, int columns, char count);
+
+#endif
